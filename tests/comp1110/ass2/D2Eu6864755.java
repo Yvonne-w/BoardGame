995dc0ff4562161762eac4c4fd75ec825c990ed4
birdy <u6864755@anu.edu.au>
2020-05-10 16:00:23 +1000

    signed my name

--- tests/comp1110/ass2/D2Eu6864755.java
+++ tests/comp1110/ass2/D2Eu6864755.java
@@ -4,9 +4,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-//public class D2Eu6864755{
+public class D2Eu6864755{
    // @Rule
    // public void test( String pieceplacement, Boolean expected){
        // boolean result；
     //}
-//}
+}
